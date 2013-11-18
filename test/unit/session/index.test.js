a9053849bf8f7331d734a943efcc879254a94d8f
Paul Jensen <paulbjensen@gmail.com>
2013-11-18 22:18:06 +0000

    Added a test file for the session channels work

---
+++ test/unit/session/index.test.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+
+describe('session index', function () {
+
+
+
+    describe('exports.options', function () {
+
+        it('should return an object containing the options for session management');
+
+    });
+
+
+
+    describe('exports.store', function () {
+
+
+
+        describe('#use', function () {
+
+            it('should set a connect-based session store for use');
+
+            it('should allow the user to specify usage of connect-redis by referencing it\'s name');
+
+        });
+
+
+
+        describe('#get', function () {
+
+            it('should return the connect-bassed session store');
+
+        });
+
+    });
+
+
+
+    describe('exports.create', function () {
+
+        it('should create a new session for the user with a unique id');
+
+    });
+
+
+
+    describe('exports.find', function () {
+
+
+
+        it('should try to find the session from the connect session store');
+
+
+
+        describe('when a session is not found', function () {
+
+            it('should create a new one');
+
+        });
+
+
+
+        describe('when a session is found', function () {
+
+            it('should attach a channel helper to the session');
+
+            it('should allow the user to set a user id for the session');
+
+            it('should bind the socket to the session');
+
+            it('should attach a save helper method for the session');
+
+        });
+
+
+
+        it('should callback with the session for that user');
+
+
+
+    });
+
+
+});
